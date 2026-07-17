35af4b44dc22cb4cbae180ffbaba45d4bf81503c
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-17 08:59:56 +0530

    make the counter app with the zustand and MUI materials

--- zustand/src/main.jsx
+++ zustand/src/main.jsx
@@ -1,10 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDom from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDom.createRoot(document.getElementById('root')).render(<App />)
