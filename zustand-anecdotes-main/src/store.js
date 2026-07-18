faa76cd686a2af9f0a023094b317a20e01b60a00
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-18 13:33:35 +0530

    6.3: anecdotes, step2

--- zustand-anecdotes-main/src/store.js
+++ zustand-anecdotes-main/src/store.js
@@ -1,35 +1,35 @@
-// import { create } from 'zustand'
+import { create } from 'zustand'
 
-// const anecdotesAtStart = [
-//   'If it hurts, do it more often',
-//   'Adding manpower to a late software project makes it later!',
-//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//   'Premature optimization is the root of all evil.',
-//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-// ]
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-// const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0)
 
-// const asObject = anecdote => ({
-//   content: anecdote,
-//   id: getId(),
-//   votes: 0
-// })
+const asObject = anecdote => ({
+  content: anecdote,
+  id: getId(),
+  votes: 0
+})
 
-// const useAnecdoteStore = create((set) => ({
-//   anecdotes: anecdotesAtStart.map(asObject),
-//   actions: {
-//     voteAnecdote: (id) => set((state) => ({
-//       anecdotes: state.anecdotes.map((a) =>
-//         a.id === id ? { ...a, votes: a.votes + 1 } : a
-//       )
-//     })),
-//     createAnecdote: (content) => set((state) => ({
-//       anecdotes: [...state.anecdotes, asObject(content)]
-//     }))
-//   }
-// }))
+const useAnecdoteStore = create((set) => ({
+  anecdotes: anecdotesAtStart.map(asObject),
+  actions: {
+    voteAnecdote: (id) => set((state) => ({
+      anecdotes: state.anecdotes.map((a) =>
+        a.id === id ? { ...a, votes: a.votes + 1 } : a
+      )
+    })),
+    createAnecdote: (content) => set((state) => ({
+      anecdotes: [...state.anecdotes, asObject(content)]
+    }))
+  }
+}))
 
-// export const useAnecdotes = () => useAnecdoteStore((state) => state.anecdotes)
-// export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions)
+export const useAnecdotes = () => useAnecdoteStore((state) => state.anecdotes)
+export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions)
