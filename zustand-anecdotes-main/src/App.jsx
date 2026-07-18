53ec41e7da7461f5138cee505580431b1a8e353d
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-18 12:24:00 +0530

    new project setup

---
+++ zustand-anecdotes-main/src/App.jsx
@@ -0,0 +1,34 @@
+
+import { useAnecdotes } from './store'
+
+const App = () => {
+  const anecdotes = useAnecdotes()
+
+  const vote = id => {
+    console.log('vote', id)
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
